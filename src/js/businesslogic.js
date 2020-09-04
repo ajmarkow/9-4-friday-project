0e504dae4ae9b525e40dfe16a413162fc5819e4d
Alexander Markow <alexanderjmarkow@gmail.com>
2020-09-04 13:29:52 -0700

    Add properties to hold list of planets, life expectancy, and age to earthuser constructor.  Write tests for each of these properties, and check that the information is stored in the earthuser object.

--- src/js/businesslogic.js
+++ src/js/businesslogic.js
@@ -1,12 +1,24 @@
 export default class Calculator {
-  earthuser(earthage, sex) {
-    this.earthage = 0;
+  earthuser() {
+    this.earthAge = 0;
     this.sex = sex;
+    this.planets = [];
     this.planetaryages = [];
+    this.lifeExpectancy = 0;
   }
 
-  function mercuryAge() {
-    let ageReturned = (earthage * 0.24)
-    return ageReturned
+  planetaryConvert() {
+    let ageArray = [];
+    let mercuryAgeReturned = parseFloat(this.earthAge * 0.24);
+    ageArray.push(mercuryAgeReturned);
+    console.log(mercuryAgeReturned);
+    let venusAgeReturned = parseFloat(this.earthAge * 0.62);
+    ageArray.push(venusAgeReturned);
+    let marsAgeReturned = parseFloat(this.earthAge * 1.88);
+    ageArray.push(marsAgeReturned);
+    let jupiterAgeReturned = parseFloat(this.earthAge * 11.86);
+    ageArray.push(jupiterAgeReturned);
+
+    return ageArray;
   }
 }
