e3c2b2f7115facabc468aa1bb34c89e46907cb5c
Alexander Markow <alexanderjmarkow@gmail.com>
2020-09-04 10:42:23 -0700

    Add failing test for user object constructor

--- __tests__/businesslogic.test.js
+++ __tests__/businesslogic.test.js
@@ -3,7 +3,7 @@ describe("person", () => {
     let testperson = new person(1, "male");
   });
 
-  test("user object stores earthage, sex properties, and planetary age array. ", () => {
+  test("user object stores earthage, sex properties, and planetary age array to hold ages. ", () => {
     expect(testperson.earthage).toEqual(1);
     expect(testperson.sex).toEqual("male");
     expect(testperson.planetaryages).toEqual([]);
